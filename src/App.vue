e74fea5128e8ce142b3049409222b6949b984ba6
junho.han <junho.han@ebridge-world.com>
2022-03-05 18:47:47 +0900

    Finish ts practice

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,9 @@
           v-for="(todoItem, i) in todoItems"
           :key="i"
           :todoItem="todoItem"
+          :index="i"
+          @remove="removeTodoItem"
+          @toggle="toggleTodoItemComplete"
         />
       </ul>
     </main>
@@ -29,41 +32,68 @@ import TodoListItem from "./components/TodoListItem.vue";
 
 const STORAGE_KEY = "vue-todo-ts-v1";
 const storage = {
-  save(todoItems: any[]) {
+  save(todoItems: Todo[]) {
     const parsed = JSON.stringify(todoItems);
     localStorage.setItem(STORAGE_KEY, parsed);
   },
-  fetch() {
+  fetch(): Todo[] {
     const todoItems = localStorage.getItem(STORAGE_KEY) || "[]";
     const result = JSON.parse(todoItems);
     return result;
   },
 };
 
+export interface Todo {
+  title: string;
+  done: boolean;
+}
 export default Vue.extend({
   components: { TodoInput, TodoListItem },
   data: () => ({
     todoText: "",
-    todoItems: [] as any,
+    todoItems: [] as Todo[],
   }),
   created() {
     this.fetchTodoItems();
   },
   methods: {
     fetchTodoItems() {
-      this.todoItems = storage.fetch();
+      this.todoItems = storage.fetch().sort((a, b) => {
+        if (a.title < b.title) {
+          return -1;
+        }
+        if (a.title > b.title) {
+          return 1;
+        }
+        return 0;
+      });
     },
     updateTodoText(value: string) {
       this.todoText = value;
     },
     addTodoItem() {
       const value = this.todoText;
-      this.todoItems.push(value);
+      const todo: Todo = {
+        title: value,
+        done: false,
+      };
+      this.todoItems.push(todo);
       storage.save(this.todoItems);
       this.initTodoText();
     },
     initTodoText() {
       this.todoText = "";
+    },
+    removeTodoItem(index: number) {
+      this.todoItems.splice(index, 1);
+      storage.save(this.todoItems);
+    },
+    toggleTodoItemComplete(todoItem: Todo, index: number) {
+      this.todoItems.splice(index, 1, {
+        ...todoItem,
+        done: !todoItem.done,
+      });
+      storage.save(this.todoItems);
     },
   },
 });
--- src/components/TodoListItem.vue
+++ src/components/TodoListItem.vue
@@ -1,19 +1,43 @@
 <template>
   <div>
-    <li>{{ todoItem }}</li>
+    <span class="item" :class="todoItemClass" @click="toggleItem">{{
+      todoItem.title
+    }}</span>
+    <button @click="removeItem">삭제</button>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-
+import { Todo } from "@/App.vue";
+import Vue, { PropType } from "vue";
 export default Vue.extend({
   props: {
     todoItem: {
-      type: String,
+      type: Object as PropType<Todo>,
+    },
+    index: Number,
+  },
+  computed: {
+    todoItemClass(): string | null {
+      return this.todoItem.done ? "complete" : null;
+    },
+  },
+  methods: {
+    toggleItem() {
+      this.$emit("toggle", this.todoItem, this.index);
+    },
+    removeItem() {
+      this.$emit("remove", this.index);
     },
   },
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.item {
+  cursor: pointer;
+}
+.complete {
+  text-decoration: line-through;
+}
+</style>
